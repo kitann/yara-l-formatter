96a8d7683259a8b2d2d41ada00dfd5cdaded13bc
SOC101 <soc101@TCM.lan>
2026-05-21 20:48:17 -0500

    Placeholder and text update

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ const versionBadge = document.querySelector("[data-version-badge]");
 const toast = document.querySelector("#toast");
 
 const appConfig = window.YARALINT_CONFIG || {
-  version: "1.2.1",
-  build: "2026-05-21T20:15:13-05:00"
+  version: "1.2.2",
+  build: "2026-05-21T20:29:19-05:00"
 };
 const sectionPattern = /^(meta|strings|events|match|outcome|condition|options):$/i;
 const githubRawBase = "https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/";
